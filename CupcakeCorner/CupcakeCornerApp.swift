fcd5277cacc29f0fd6df95a1f5e98d3172043181
Brandon Knox <bknox.dev@gmail.com>
2021-04-14 06:46:01 -0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Brandon Knox on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
