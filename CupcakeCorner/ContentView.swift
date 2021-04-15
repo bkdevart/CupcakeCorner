8befc1a74423901b20137450e2a323b79c93491d
Brandon Knox <bknox.dev@gmail.com>
2021-04-15 16:29:04 -0700

    Embedded main ContentView in Navigation view to enable Picker and NavigationLink sections

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -12,42 +12,44 @@ struct ContentView: View {
     @ObservedObject var order = Order()
     
     var body: some View {
-        Form {
-            Section {
-                Picker("Select your cake type", selection: $order.type) {
-                    ForEach(0..<Order.types.count) {
-                        Text(Order.types[$0])
-                    }
-                }
-                
-                Stepper(value: $order.quantity, in: 3...20) {
-                    Text("Number of cakes: \(order.quantity)")
-                }
-            }
-            
-            Section {
-                Toggle(isOn: $order.specialRequestEnabled.animation()) {
-                    Text("Any special requests?")
-                }
-                
-                if order.specialRequestEnabled {
-                    Toggle(isOn: $order.extraFrosting) {
-                        Text("Add extra frosting")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
                     }
                     
-                    Toggle(isOn: $order.addSprinkles) {
-                        Text("Add extra sprinkles")
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
                     }
                 }
             }
-            
-            Section {
-                NavigationLink(destination: AddressView(order: order)) {
-                    Text("Delivery details")
-                }
-            }
+            .navigationBarTitle("Cupcake Corner")
         }
-        .navigationBarTitle("Cupcake Corner")
     }
 }
     
