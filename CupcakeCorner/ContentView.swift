913aef65f90922f1246064e756a2ad4f35d68702
Brandon Knox <bknox.dev@gmail.com>
2021-04-15 05:36:51 -0700

    Started form with cake selection and number of cakes

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -12,7 +12,20 @@ struct ContentView: View {
     @ObservedObject var order = Order()
     
     var body: some View {
-        Text("Hello world")
+        Form {
+            Section {
+                Picker("Select your cake type", selection: $order.type) {
+                    ForEach(0..<Order.types.count) {
+                        Text(Order.types[$0])
+                    }
+                }
+                
+                Stepper(value: $order.quantity, in: 3...20) {
+                    Text("Number of cakes: \(order.quantity)")
+                }
+            }
+        }
+        .navigationBarTitle("Cupcake Corner")
     }
 }
     
